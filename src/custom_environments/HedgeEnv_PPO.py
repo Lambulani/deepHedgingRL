d33a225dc5555665a7b4845ca3b5a9346a24f0d4
Lambulani <levybanda23@gmail.com>
2024-10-17 21:37:35 +0200

    Working copy of PPO discrete actions space. Now trying hyperparameter search

--- src/custom_environments/HedgeEnv_PPO.py
+++ src/custom_environments/HedgeEnv_PPO.py
@@ -1,4 +1,5 @@
-import gym
+import gymnasium as gym
+
 import numpy as np
 from gym import Env, spaces
 from gym.utils import seeding
@@ -9,10 +10,10 @@ from gym.utils import seeding
 #define the hedging object 
 class env_hedging_ppo(Env):
     metadata = {'render.modes': ['human']}
-    def __init__(self, asset_price_model, dt, T, num_steps=100, cost_multiplier = 0.005, tick_size=0.01,
-                 L=1, strike_price=100, integer_holdings =True, initial_holding=0, mode="PL",  **kwargs):
+    def __init__(self, asset_price_model, dt, T, num_steps= 50, cost_multiplier = 0, tick_size=0.01,
+                 L=1, strike_price=100, integer_holdings =True, initial_holding=0, mode="PL", shares_per_contract =100,**kwargs):
         super().__init__()
-        self.action_space = spaces.Discrete(201)
+        self.action_space = spaces.Box(low=-1, high=1, shape=(1,), dtype=np.float32)
         self.observation_space = spaces.Box(low=0, high=np.inf, shape=(5,), dtype=np.float32)
         assert (mode in ["PL", "CF"]), "Only 'PL' and 'CL' are allowed values for mode."
         self.asset_price_model = asset_price_model
@@ -20,6 +21,7 @@ class env_hedging_ppo(Env):
         self.n = 0
         self.T = T
         self.dt = dt
+        self.shares_per_contract = shares_per_contract
         if mode == "PL":
             self.option_price_model = kwargs.get('option_price_model', None)
             assert (self.option_price_model is not None), "If 'PL' is chosen a option_price_model needs be be provided."
@@ -30,7 +32,7 @@ class env_hedging_ppo(Env):
         self.mode = mode
         self.L = L
         self.initial_holding = initial_holding
-        self.h = initial_holding
+        self.h = -self.shares_per_contract*self.delta
         self.integer_holdings  = integer_holdings 
         if integer_holdings :
             self.h = round(self.h)
@@ -40,33 +42,36 @@ class env_hedging_ppo(Env):
             self.strike_price = self.current_price
         self.tick_size = tick_size
         self.cost_multiplier = cost_multiplier
-    def _compute_cf_reward(self, new_h, next_price, delta_h):
-        reward = -self.current_price * delta_h - self.cost_multiplier * self.tick_size* np.abs(self.current_price * delta_h)
-        if self.done:
-            asset_value = next_price * new_h - self.cost_multiplier *self.tick_size * np.abs(next_price * new_h)
-            payoff = self.L *100* max(0, next_price - self.strike_price)
-            reward += asset_value - payoff
-        return reward 
-    def _compute_pl_reward(self, old_h, next_price, delta_h):
-            new_option_price = self.option_price_model.compute_option_price(self.n, next_price)
-            reward_option_price = self.L * (100*(new_option_price - self.current_option_price) + old_h*
-                                                (next_price - self.current_price))
-            trading_cost =  self.cost_multiplier* self.tick_size * (abs(delta_h) + 0.01 * delta_h**2)
-            delta_wealth = reward_option_price - trading_cost
-            self.current_option_price = new_option_price
-            
-            if self.done:
-                asset_value = next_price * (old_h + delta_h)  - self.cost_multiplier* self.tick_size * (abs(old_h + delta_h) + 0.01 * (old_h + delta_h)**2)
-                payoff = self.L *100* max(0, next_price - self.strike_price)
-                delta_wealth = delta_wealth + asset_value + payoff
-
-            reward = delta_wealth - (0.1/2)*(delta_wealth**2) #reward function according to Kolm (2019), quadratic utility mean variance optimization 
-            reward = reward/1e6 #scaled reward
+    # def _compute_cf_reward(self, new_h, next_price, delta_h):
+    #     reward = -self.current_price * delta_h - self.cost_multiplier * self.tick_size* np.abs(self.current_price * delta_h)
+    #     if self.done:
+    #         asset_value = next_price * new_h - self.cost_multiplier *self.tick_size * np.abs(next_price * new_h)
+    #         payoff = self.L *100* max(0, next_price - self.strike_price)
+    #         reward += asset_value - payoff
+    #     return reward 
+    def _compute_pl_reward(self, prev_h, next_price, delta_h):
+        if self.n ==0 :
+            reward =-self.cost_multiplier* self.tick_size * (abs(prev_h) + 0.01 * prev_h**2)
             return reward
+        new_option_price = self.option_price_model.compute_option_price(self.n, next_price)
+        delta_V = self.L * self.shares_per_contract*(new_option_price - self.current_option_price)
+        delta_S = prev_h*(next_price - self.current_price)
+        trading_cost =  self.cost_multiplier* self.tick_size * (abs(delta_h) + 0.01 * delta_h**2)
+        delta_wealth = delta_V + delta_S - trading_cost
+        self.current_option_price = new_option_price
+        
+        if self.done:
+            asset_value = next_price * (prev_h + delta_h) 
+            termination_cost = self.cost_multiplier* self.tick_size * (abs(prev_h + delta_h) + 0.01 * (prev_h + delta_h)**2)
+            payoff = self.L *self.shares_per_contract* max(0, next_price - self.strike_price)
+            delta_wealth = delta_wealth +  payoff +asset_value - termination_cost
+
+        reward = delta_wealth - (0.1/2)*(delta_wealth**2) #reward function according to Kolm (2019), quadratic utility mean variance optimization 
+        return reward
 
     def step(self, delta_h):
-        action_mapping = np.arange(-100, 101) 
-        delta_h = action_mapping[delta_h]
+         
+        delta_h = 100*delta_h[0]
         if self.integer_holdings :
             delta_h = round(delta_h)
         new_h = self.h + delta_h
@@ -79,7 +84,7 @@ class env_hedging_ppo(Env):
         if self.mode == "CF":
             reward = self._compute_cf_reward(new_h, next_price, delta_h)
         elif self.mode == "PL":
-            reward = self._compute_pl_reward(self.h, next_price, delta_h)
+            reward = self._compute_pl_reward(self.h, next_price, delta_h)/1e5
             self.delta = self.option_price_model.compute_delta(self.n, self.current_price)
         else:
             assert "error 1"
@@ -92,7 +97,7 @@ class env_hedging_ppo(Env):
     def get_state(self):
         time_to_maturity = self.T - self.n * self.dt
         if self.mode == "PL":
-            return np.array([self.h, self.current_price, time_to_maturity, self.current_option_price, self.delta],
+            return np.array([self.h, self.current_price, time_to_maturity, self.current_option_price*self.shares_per_contract, self.delta*self.shares_per_contract],
                             dtype=float)
         else:
             return np.array([self.h, self.current_price, time_to_maturity], dtype=float)
@@ -105,12 +110,15 @@ class env_hedging_ppo(Env):
         self.delta= delta 
     
     def seed(self, seed=None):
+        if seed is None:
+            seed = np.random.randint(0, 2**31-1)
+
         self.np_random, seed = seeding.np_random(seed)
-        return [seed]
+        return seed
 
 
-    def reset(self,seed = 2024, options = None, **kwargs):
-        super().reset(seed =seed) 
+    def reset(self, seed = None, options = None):
+        super().reset(seed = seed) 
         self.seed(seed)
         self.asset_price_model.reset()
         self.n = 0
@@ -128,10 +136,6 @@ class env_hedging_ppo(Env):
 
     def render(self, mode='human', close=False):
         return self.get_state()
-
-
-
-
 
 
 
---
+++ src/drl_agents/PPO.py
@@ -0,0 +1,175 @@
+import sys 
+sys.path.append("/home/bndlev001/deepHedgingRL")
+import gymnasium as gym
+import numpy as np
+import torch as th
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from src.pricing.asset_price_process import GBM
+from src.pricing.option_price_process import BSM
+from src.custom_environments.HedgeEnv_PPO import env_hedging_ppo
+from torch import nn
+
+
+mu = 0
+dt = 1/5
+T = 10
+num_steps = int(T/dt)
+s_0 = float(100)
+strike_price = s_0
+sigma = 0.01
+r = 0
+
+
+apm = GBM(mu=mu, dt=dt, s_0=s_0, sigma=sigma)
+opm = BSM(strike_price=strike_price, risk_free_interest_rate=r, volatility=sigma, T=T, dt=dt)
+
+env = env_hedging_ppo(asset_price_model=apm, dt=dt, T=T, num_steps=num_steps, cost_multiplier = 0, tick_size=0.01,
+                     L=1, strike_price=strike_price, integer_holdings=True, initial_holding=0, mode="PL", shares_per_contract=100,
+                  option_price_model=opm)
+
+
+
+
+from stable_baselines3 import PPO
+from gym import spaces
+from typing import Callable, Tuple
+from stable_baselines3.common.policies import ActorCriticPolicy
+from stable_baselines3.common.utils import get_schedule_fn
+from stable_baselines3.common.env_util import make_vec_env
+from torch.nn import functional as F
+import os 
+
+
+n_envs = 10
+vec_env = make_vec_env(lambda:env_hedging_ppo(asset_price_model=apm, dt=dt, T=T, num_steps=num_steps, cost_multiplier = 0, tick_size=0.01,
+                     L=1, strike_price=strike_price, integer_holdings=True, initial_holding=0, mode="PL",
+                  option_price_model=opm) , n_envs= n_envs)
+
+
+
+class Du_Custom_Network(nn.Module):
+    
+    def __init__(self, feature_dim: int = 5,last_layer_dim_pi: int = 201,last_layer_dim_vf: int = 1, hidden_dim: int = 64):
+        super().__init__()
+
+        # Save output dimensions, used to create the distributions
+        self.hidden_dim = hidden_dim
+        self.latent_dim_pi = last_layer_dim_pi
+        self.latent_dim_vf = last_layer_dim_vf
+
+        # Policy network with 5 hidden layers, batch normalization before ReLU
+        self.policy_net = nn.Sequential(
+            
+            nn.Linear(feature_dim, hidden_dim),
+            nn.BatchNorm1d(hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.BatchNorm1d(hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.BatchNorm1d(hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.BatchNorm1d(hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, last_layer_dim_pi)
+        )
+
+        # Value network with 5 hidden layers, batch normalization before ReLU
+        self.value_net = nn.Sequential(
+            nn.Linear(feature_dim, hidden_dim),
+            nn.BatchNorm1d(hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.BatchNorm1d(hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.BatchNorm1d(hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, hidden_dim),
+            nn.BatchNorm1d(hidden_dim),
+            nn.ReLU(),
+            nn.Linear(hidden_dim, last_layer_dim_vf)
+        )
+
+    def forward(self, features: th.Tensor) -> Tuple[th.Tensor, th.Tensor]:
+        """
+        :return: (th.Tensor, th.Tensor) latent_policy, latent_value of the specified network.
+            If all layers are shared, then ``latent_policy == latent_value``
+        """
+        return self.forward_actor(features), self.forward_critic(features)
+
+    def forward_actor(self, features: th.Tensor) -> th.Tensor:
+        logits = self.policy_net(features)
+        return F.softmax(logits, dim=-1) 
+
+    def forward_critic(self, features: th.Tensor) -> th.Tensor:
+        return self.value_net(features)
+
+
+class Du_ActorCriticPolicy(ActorCriticPolicy):
+    def __init__(
+        self,
+        observation_space: spaces.Space,
+        action_space: spaces.Space,
+        lr_schedule: Callable[[float], float],
+        *args,
+        **kwargs,
+    ):
+        # Disable orthogonal initialization
+        kwargs["ortho_init"] = False
+        super().__init__(
+            observation_space,
+            action_space,
+            lr_schedule,
+            *args,
+            **kwargs,
+        )
+
+    def _build_mlp_extractor(self) -> None:
+        self.mlp_extractor =Du_Custom_Network(self.features_dim)
+
+
+# Set up directories for logging and saving models
+log_dir = "/home/bndlev001/deepHedgingRL/results/PPO/du/logs"
+
+models_dir = "/home/bndlev001/models/PPO/du"
+if not os.path.exists(models_dir):
+    os.makedirs(models_dir)
+
+def linear_decay_schedule(initial_value: float, final_value:float) -> Callable[[float], float]:
+    def schedule(progress: float) -> float:
+        # Linearly decay the learning rate from `initial_value` to 0
+        return initial_value - progress*(initial_value- final_value)
+    return schedule
+
+learning_rate_schedule = get_schedule_fn(linear_decay_schedule(1e-4, 1e-5))
+
+# Instantiate and train the PPO model as per the Du paper
+model = PPO(
+    policy= Du_ActorCriticPolicy, 
+    env= vec_env, 
+    learning_rate=1e-4,
+    n_steps= 1500, #update every 15000 episodes
+    n_epochs = 5, 
+    clip_range =0.2, 
+    verbose=1,
+    gae_lambda=0.99, 
+    gamma=0.85,
+    ent_coef=0.2,  # Entropy coefficient for exploration #check c1 parameter and c2 parameter 0
+    vf_coef=0.5,  # Value function coefficient
+    normalize_advantage= True,
+    tensorboard_log=log_dir,
+    device= "auto"
+)
+
+
+# Train the model in increments and save after each block of timesteps
+TIMESTEPS = 1000000
+for i in range(1,20):
+    model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name="PPO")
+    model.save(f"{models_dir}/ppo_{TIMESTEPS*i}")
+
+
+
